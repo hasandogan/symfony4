e7a8cf3cecc073764b4e5e857132135cf329c877
Hasan Doğan <hasandoganx@gmail.com>
2020-08-13 15:16:23 +0300

    making so much good progres

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+
+        return new Response('My first page');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new  Response(sprintf(
+            'future page to show one space article ',
+            $slug
+
+        ));
+    }
+
+}
